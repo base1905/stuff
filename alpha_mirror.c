d67778a2151af7295c0bbc8c73f8562b9a84d63e
arannara <base1905@yandex.ru>
2020-11-17 01:11:26 +0300

    decide alpha

--- alpha_mirror.c
+++ alpha_mirror.c
@@ -1,16 +1,29 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	int i;
+/* not printing simbols different from a-z, A-Z and space */
 
+	int i;
+	int new_i;
 	i = 0;
 	if (argc == 2)
 	{
 		while(argv[1][i])
 		{
-			if (argv[1][i] >= 65 && argv[i][i] <= 90)
-				i = 100 - i + 48
+			if (argv[1][i] >= 65 && argv[1][i] <= 90)
+			{
+				new_i = argv[1][i] * -1  + 155;
+				write(1, &new_i, 1);
+			}
+			else if (argv[1][i] >= 97 && argv[1][i] <= 122)
+			{
+				new_i = argv[1][i] * -1  + 219;
+				write(1, &new_i, 1);
+			}
+			else if (argv[1][i] == ' ')
+				write(1, " ", 1);
 			i++;
 		}
 	}
